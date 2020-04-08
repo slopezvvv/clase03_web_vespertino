c957b55ab5c049f9ebfe1da364e05bdf5f99e1b5
yo <yo@DESKTOP-RVOAHLO>
2020-04-07 22:45:57 -0400

    initial commit.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/estilo_index.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <!--
+        Cuadrado grande
+        -->
+        <div id="cuadrado_grande">
+            <!-- La linea superior -->
+            <div id="linea_superior">
+                <div class="clase_rojo"></div>
+                <div class="clase_rojo"></div>
+                <div class="clase_rojo"></div>
+            </div>
+            <!-- La linea del medio -->
+            <div id="linea_medio">
+                <div class="clase_verde"></div>
+                <div class="clase_verde"></div>
+                <div class="clase_verde"></div>
+            </div>
+            <!-- La linea del final -->
+            <div id="linea_final">
+                <div class="clase_azul"></div>
+                <div class="clase_azul"></div>
+                <div class="clase_azul"></div>
+            </div>
+        </div>
+    </body>
+</html>
